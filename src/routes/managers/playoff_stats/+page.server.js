d87347600b94d9e5b57eaf4cba09e37b701ea44a
michaelh440 <michael.hyland@yahoo.com>
2025-09-24 09:33:15 -0500

    updated manager playoff page to use the playoff view instead of reg_season view

--- src/routes/managers/playoff_stats/+page.server.js
+++ src/routes/managers/playoff_stats/+page.server.js
@@ -83,17 +83,16 @@ export async function load({ url }) {
   };
 }*/
 
-// src/routes/league/reg_season_stats/+page.server.js
 import { query } from '$lib/db';
 
 export async function load() {
-  const highestGame   = (await query(`SELECT * FROM public.vw_reg_season_highest_game`)).rows;
-  const lowestGame    = (await query(`SELECT * FROM public.vw_reg_season_lowest_game`)).rows;
-  const highestSeason = (await query(`SELECT * FROM public.vw_reg_season_highest_season`)).rows;
-  const lowestSeason  = (await query(`SELECT * FROM public.vw_reg_season_lowest_season`)).rows;
-  const blowout       = (await query(`SELECT * FROM public.vw_reg_season_largest_blowout`)).rows;
-  const nailbiter     = (await query(`SELECT * FROM public.vw_reg_season_closest_nailbiter`)).rows;
-  const winPct        = (await query(`SELECT * FROM public.vw_reg_season_win_pct`)).rows;
+  const highestGame   = (await query(`SELECT * FROM vw_playoff_highest_game`)).rows;
+  const lowestGame    = (await query(`SELECT * FROM vw_playoff_lowest_game`)).rows;
+  const highestSeason = (await query(`SELECT * FROM vw_playoff_highest_season`)).rows;
+  const lowestSeason  = (await query(`SELECT * FROM vw_playoff_lowest_season`)).rows;
+  const blowout       = (await query(`SELECT * FROM vw_playoff_largest_blowout`)).rows;
+  const nailbiter     = (await query(`SELECT * FROM vw_playoff_closest_nailbiter`)).rows;
+  const winPct        = (await query(`SELECT * FROM vw_playoff_win_pct`)).rows;
 
   return {
     highestGame,
