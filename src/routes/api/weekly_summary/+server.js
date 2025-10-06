915dac0a0eba052a51abf666dee843cc43b70d55
michaelh440 <michael.hyland@yahoo.com>
2025-10-05 20:36:36 -0500

    corrected weekly summary query to pull correct team names

--- src/routes/api/weekly_summary/+server.js
+++ src/routes/api/weekly_summary/+server.js
@@ -23,22 +23,22 @@ export async function GET({ url }) {
                     
                     -- Team 1 info
                     m.team1_id,
-                    COALESCE(mtn1.team_name, t1.team_name) as team1_name,
+                    COALESCE(mtn1.team_name, mgr1.username) as team1_name,
                     mgr1.manager_id as manager1_id,
                     mgr1.username as manager1_name,
                     m.team1_score,
                     
                     -- Team 2 info
                     m.team2_id,
-                    COALESCE(mtn2.team_name, t2.team_name) as team2_name,
+                    COALESCE(mtn2.team_name, mgr2.username) as team2_name,
                     mgr2.manager_id as manager2_id,
                     mgr2.username as manager2_name,
                     m.team2_score,
                     
                     -- Winner calculation
                     CASE 
-                        WHEN m.team1_score > m.team2_score THEN COALESCE(mtn1.team_name, t1.team_name)
-                        WHEN m.team2_score > m.team1_score THEN COALESCE(mtn2.team_name, t2.team_name)
+                        WHEN m.team1_score > m.team2_score THEN COALESCE(mtn1.team_name, mgr1.username)
+                        WHEN m.team2_score > m.team1_score THEN COALESCE(mtn2.team_name, mgr2.username)
                         ELSE 'TIE'
                     END as winner,
                     ABS(m.team1_score - m.team2_score) as margin
@@ -46,14 +46,13 @@ export async function GET({ url }) {
                 FROM matchups m
                 JOIN seasons s ON m.season_id = s.season_id
                 
-                -- Join to teams to get manager_id
-                JOIN teams t1 ON m.team1_id = t1.team_id
-                JOIN managers mgr1 ON t1.manager_id = mgr1.manager_id
+                -- Join DIRECTLY to managers (team1_id = manager_id)
+                JOIN managers mgr1 ON m.team1_id = mgr1.manager_id
                 LEFT JOIN manager_team_names mtn1 ON mtn1.manager_id = mgr1.manager_id 
                     AND mtn1.season_year = s.season_year
                 
-                JOIN teams t2 ON m.team2_id = t2.team_id
-                JOIN managers mgr2 ON t2.manager_id = mgr2.manager_id
+                -- Join DIRECTLY to managers (team2_id = manager_id)
+                JOIN managers mgr2 ON m.team2_id = mgr2.manager_id
                 LEFT JOIN manager_team_names mtn2 ON mtn2.manager_id = mgr2.manager_id 
                     AND mtn2.season_year = s.season_year
                     
